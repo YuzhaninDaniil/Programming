422f37e7e4d440369dcb5e9593db49c1ffa306e8
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2024-06-20 14:59:58 +0700

    Update Program.cs

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -6,7 +6,7 @@ namespace Programming
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() // п
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
