440162dc2187be62b9918252d6b9324b7a51c605
danYuzh05 <weltrups@gmail.com>
2025-04-22 12:56:36 +0700

    did 4 lab.work

---
+++ src/NewContactsApp/View/Controls/ContactControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace View.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        private static readonly Regex PhoneNumberRegex =
+            new Regex(@"^[0-9+\-()\s]*$", RegexOptions.Compiled);
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обрабатывает ввод символов в поле телефонного номера,
+        /// разрешая только цифры, плюсы, скобки, дефисы и пробелы
+        /// </summary>
+        private void PhoneNumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            string newText = textBox.Text + e.Text;
+
+            bool isValidInput = PhoneNumberRegex.IsMatch(e.Text) && newText.Length <= 100;
+
+            e.Handled = !isValidInput;
+        }
+
+        /// <summary>
+        /// Обрабатывает вставку текста в поле телефонного номера, проверяя допустимость вставляемых данных
+        /// </summary>
+        private void PhoneNumberTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(DataFormats.Text))
+            {
+                string text = (string)e.DataObject.GetData(DataFormats.Text);
+                var textBox = (TextBox)sender;
+                string newText = textBox.Text + text;
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    bool isValidPaste = PhoneNumberRegex.IsMatch(text) && newText.Length <= 100;
+
+                    if (!isValidPaste)
+                    {
+                        e.CancelCommand();
+                    }
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
--- src/NewContactsApp/View/Model/Services/ContactSerializer.cs
+++ src/NewContactsApp/View/Model/Services/ContactSerializer.cs
@@ -1,23 +1,24 @@
-﻿using System.IO;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 using Newtonsoft.Json;
 
-namespace View.Model.Servicies
+namespace View.Model.Services
 {
     /// <summary>
     /// Класс, отвечающий за сериализацию и десериализацию объектов типа <see cref="Contact"/>.
     /// </summary
-    public class ContactSerializer
+    public static class ContactSerializer
     {
         /// <summary>
         /// Хранит и возвращает путь к файлу, в котором хранятся данные контакта.
         /// </summary>
-        public string FilePath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Contacts", "contacts.json");
+        public static string FilePath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Contacts", "contacts.json");
 
         /// <summary>
         /// Создает новый экземпляр класса <see cref="ContactSerializer"/>.
         /// Устанавливает путь к файлу.
         /// </summary>
-        public ContactSerializer()
+        static ContactSerializer()
         {
             if (!Directory.Exists(Path.GetDirectoryName(FilePath)))
                 Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
@@ -27,7 +28,7 @@ namespace View.Model.Servicies
         /// Сохраняет контакты в файл в формате JSON.
         /// </summary>
         /// <param name="contacts">Список контактов для сохранения.</param>
-        public void SaveContacts(IEnumerable<Contact> contacts)
+        public static void SaveContacts(IEnumerable<Contact> contacts)
         {
             string json = JsonConvert.SerializeObject(contacts, Formatting.Indented);
             File.WriteAllText(FilePath, json);
@@ -37,22 +38,22 @@ namespace View.Model.Servicies
         /// Загружает контакты из файла.
         /// </summary>
         /// <returns>Список контактов.</returns>
-        public List<Contact> LoadContacts()
+        public static ObservableCollection<Contact> LoadContacts()
         {
             try
             {
                 if (!File.Exists(FilePath))
                 {
-                    return new List<Contact>();
+                    return new ObservableCollection<Contact>();
                 }
 
                 string json = File.ReadAllText(FilePath);
-                List<Contact> contacts = JsonConvert.DeserializeObject<List<Contact>>(json);
+                ObservableCollection<Contact> contacts = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(json);
                 return contacts;
             }
             catch (Exception ex)
             {
-                return new List<Contact>();
+                return new ObservableCollection<Contact>();
             }
         }
     }
